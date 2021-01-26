4a317186882b327b8845c4ae6510e51908d12bc8
Arkhip <carielis.easy@gmail.com>
2021-01-27 00:27:54 +0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { AppModule } from './app.module';
 
 const port = process.env.PORT || 8000;
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
   app.enableCors();
+  await app.listen(port);
+
   Logger.log(`Server start on http:/localhost:${port}`, 'URL');
   Logger.log(process.env.SECRET);
 }
